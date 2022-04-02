28603d588437b892fe1f1b5303a9f5ecd0e9d978
j-guerreiro <joel.borghi@gmail.com>
2022-04-02 19:30:05 -0300

    update - adicionando botao de submit no form, adicionados icones nos titulos

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Hubballi&family=Work+Sans:wght@300;400;700&display=swap" rel="stylesheet"> 
+  <link href="https://fonts.googleapis.com/css2?family=Hubballi&family=Work+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet"> 
   <link rel="stylesheet" href="style.css">
   <title>Calculadora de Combustível</title>
 </head>
@@ -16,29 +17,35 @@
     <div class="calculadora">
       <!--  primeiro conteiner | descricao -->
       <div class="descricao">
-        <h1 class="titulo">Instruções</h1>
+        <h1 class="titulo"><i class="ri-gas-station-fill"></i>  Instruções  </h1>
         <div class="descricao-inner">
           <p class="paragrafo">Esta aplicação tem como finalidade demonstrar os valores  que serão gastos 
             com combustível durante um viagem. 
             Os valores tem como base o consumo do seu veículo, e a distância a ser percorrida. 
           </p>
           <ul class="lista-combustives">
-            <li>Álcool</li>
-            <li>Diesel</li>
-            <li>Gasolina</li>
+            <li class="tipo">Álcool</li>
+            <li class="tipo">Diesel</li>
+            <li class="tipo">Gasolina</li>
           </ul>
         </div>
       </div>
       <!-- segundo conteiner | formulário -->
       <div class="form-calculadora">
-        <h1 class="titulo">Cálculo do Valor(R$) do consumo</h1>
-        <div class="form-inner"></div>
+        <h1 class="titulo"><i class="ri-bus-2-fill"></i>   Cálculo do Valor(R$) do consumo</h1>
+        <div class="form-inner">
         <form>
           <label for="distancia" name="distancia" min="1" max="1000000">
           <input type="number" id="distancia">Distância em quilômetros a ser percorrida</input>
+          
           <label for="distancia" name="distancia" min="1" max="1000000">
-          <input type="number" id="distancia">Distância em quilômetros a ser percorrida</input> 
+          <input type="number" id="distancia">Consumo de combustível do veículo em (km/l)</input>
+
+          <label for="calcular" name="distancia">
+          <input type="submit" id="calcular" value="Calcular"></input>
+
         </form>
+        </div>
       </div>
     </div>
   </main>
