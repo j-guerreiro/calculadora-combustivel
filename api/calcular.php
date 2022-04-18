28d3634af6ad3142596674403e20d886f66dc13c
j-guerreiro <joel.borghi@gmail.com>
2022-04-18 17:17:37 -0300

    feature - separando o código em funções com responsabilidades próprias

--- api/calcular.php
+++ api/calcular.php
@@ -1,67 +1,77 @@
 <?php
 
+function printResultado($distancia, $consumo, $tipoCombustivel,$totalGasto){
+  
+  $saidaCalculo =<<<MENSAGEM
+
+    <p style='padding:20px;'>
+    <b>Distância a percorrer:</b> {$distancia}km <br> 
+    <b>Consumo do veículo:</b> {$consumo}Km/l <br> 
+    <b>Combustível:</b> {$tipoCombustivel}<br> 
+    <b>Total:</b> R$ {$totalGasto}</p>
+
+  MENSAGEM;
+
+  return $saidaCalculo;
+
+};
+
+function verificaCombustivel($tipoCombustivel, $litros, $distancia, $consumo){
+
+  switch($tipoCombustivel){
+
+    case 'gasolina':
+      $tipoCombustivel = 'Gasolina';
+      $precoLitro = 7.24;
+      $totalGasto = round($precoLitro * $litros, 2);
+      return printResultado($distancia, $consumo, $tipoCombustivel, $totalGasto);
+      break;
+    
+    case 'alcool':
+      $tipoCombustivel = 'Álcool';
+      $precoLitro = 5.04;
+      $totalGasto = round($precoLitro * $litros, 2);
+      return printResultado($distancia, $consumo, $tipoCombustivel, $totalGasto);
+      break;
+
+    case 'diesel':
+      $tipoCombustivel = 'Diesel';
+      $precoLitro = 5.89;
+      $totalGasto = round($precoLitro * $litros, 2);
+      return printResultado($distancia, $consumo, $tipoCombustivel, $totalGasto);
+      break;
+  };
+
+}
+
 if($_POST) {
 
-  $distancia =  $_REQUEST['distancia'];
-  $consumo =  $_REQUEST['consumo'];
+  $distancia = $_REQUEST['distancia'];
+  $consumo = $_REQUEST['consumo'];
   $tipoCombustivel = $_REQUEST['combustivel'];
 
   if(is_numeric($distancia) && is_numeric($consumo)) {
     if($distancia > 0 && $consumo > 0) {
-      $litros = $distancia / $consumo;
-
-      switch($tipoCombustivel){
-
-        case 'gasolina':
-          $tipoCombustivel = 'Gasolina';
-          $precoLitro = 7.24;
-          $totalGasto = round($precoLitro * $litros, 2);
-          break;
-        
-        case 'alcool':
-          $tipoCombustivel = 'Álcool';
-          $precoLitro = 5.04;
-          $totalGasto = round($precoLitro * $litros, 2);
-          break;
-
-        case 'diesel':
-          $tipoCombustivel = 'Diesel';
-          $precoLitro = 5.89;
-          $totalGasto = round($precoLitro * $litros, 2);
-      }
-
-      /*
-      if($tipoCombustivel == 'gasolina') {
-        $tipoCombustivel = 'Gasolina';
-        $precoLitro = 7.24;
-        $totalGasto = round($precoLitro * $litros, 2);
-      }
-      else if ($tipoCombustivel == 'alcool') {
-        $tipoCombustivel = 'Álcool';
-        $precoLitro = 5.04;
-        $totalGasto = round($precoLitro * $litros, 2);
-      }
-      else if($tipoCombustivel == 'diesel') {
-        $tipoCombustivel = 'Diesel';
-        $precoLitro = 5.89;
-        $totalGasto = round($precoLitro * $litros, 2);
-      } */
-
       
-      $mensagem = "<p style='padding:20px;'><b>Distância a percorrer:</b> {$distancia}km <br> <b>Consumo do veículo:</b> {$consumo}Km/l 
-      <br> <b>Combustível:</b> {$tipoCombustivel}<br> <b>Total:</b> R$ {$totalGasto}</p>";
+      $litros = $distancia / $consumo;
+      $mensagem = verificaCombustivel($tipoCombustivel, $litros, $distancia,$consumo);
+    
     }
-    else {
+    else 
+    {
       $mensagem = "<p style='color:red; font-weight:bold; padding:20px;'>Dados inválidos!</p>";
     }   
   }
-  else {
+  else 
+  {
     $mensagem = "<p style='color:red; font-weight:bold; padding:20px;'>Dados inválidos!</p>";
   }
 }
-else {
+else 
+{
   $mensagem = "<p style='color:red; font-weight:bold; padding:20px;'>Não foi possível realizar o cálculo!</p>";
 }
+
 ?>
 
 <!DOCTYPE html>
