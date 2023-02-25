87a2c3210bb59fc1ecb774f43dcab59dcf09bf9a
jguerreiro <joel.borghi@gmail.com>
2023-02-25 14:57:23 -0300

    [feature/refactoring-01] Separating html files in different templates

---
+++ api/header.php
@@ -0,0 +1,26 @@
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Hubballi&family=Work+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet"> 
+    <link rel="stylesheet" href="style.css">
+    <title>Calculadora de Combustível</title>
+  </head>
+  <body>
+    <main>
+    <!--  conteiner app -->
+      <div class="calculadora">
+        <!--  primeiro conteiner | descricao -->
+        <div class="descricao">
+          <h1 class="titulo"><i class="ri-gas-station-fill ri-xl"></i>  Instruções  </h1>
+          <div class="descricao-inner">
+            <p class="paragrafo">Esta aplicação tem como finalidade demonstrar os valores  que serão gastos 
+              com combustível durante uma viagem. 
+              Os valores tem como base o consumo do seu veículo, e a distância a ser percorrida. 
+            </p>
+          </div>
+        </div>
